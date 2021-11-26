ff458c9ca98d6077606797c26aabc933773f18a5
m3rcury02 <gunal2002@gmail.com>
2021-11-27 03:47:37 +0530

    text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,10 +14,10 @@ class Navbar extends Component {
           rel="noopener noreferrer"
         >
           <img src={box} width="30" height="30" className="align-top" alt="" />
-          BlockStore
+          StoreBlock
         </a>
         <ul className="navbar-nav px-3">
-          <b className='text-white'>{'VIT'}</b>
+          <b className='text-white'>{'for VIT'}</b>
         </ul>
       </nav>
     );
